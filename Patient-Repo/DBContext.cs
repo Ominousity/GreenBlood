651459b275af5c46cff181f4b391dfcfc1362c8f
Casper Fink <47432671+zeroject@users.noreply.github.com>
2024-04-27 21:32:39 +0200

    env not sets the database url

--- Patient-Repo/DBContext.cs
+++ Patient-Repo/DBContext.cs
@@ -12,16 +12,11 @@ namespace Patient_Repo
     public class DBContext : DbContext
     {
         public DbSet<PatientBe> Patients { get; set; }
+
         public DBContext()
         {
-
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //TODO move connection string to appsettings.json
-            optionsBuilder.UseNpgsql("Host=patient-db;Port=5432;Database=patientdb;Username=postgres;Password=password");
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<PatientBe>().HasKey(p => p.SSN);
