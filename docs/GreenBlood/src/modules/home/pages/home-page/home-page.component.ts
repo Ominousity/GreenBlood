b275869cc4aa22dd8fb2081737fa51f584fb2117
Anders Senger <91528409+Ominousity@users.noreply.github.com>
2024-04-28 01:17:52 +0200

    geolocking

--- docs/GreenBlood/src/modules/home/pages/home-page/home-page.component.ts
+++ docs/GreenBlood/src/modules/home/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,6 +15,37 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
 
+  lat: number = 0;
+  lng: number = 0;
+
+  approved: boolean = false;
+
+  ngOnInit(): void {
+    this.getLocation();
+    
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        if (position) {
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          console.log(this.lat);
+          console.log(this.lat);
+        }
+
+        //if position is within denmark set approved to true
+        if (this.lat > 54.5 && this.lat < 57.5 && this.lng > 8 && this.lng < 15) {
+          this.approved = true;
+        }
+
+      },
+        (error) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
